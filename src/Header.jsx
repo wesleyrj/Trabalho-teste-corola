9369842830843b6e89cc48fe4f74e113bae8a8ac
wesleyrj <115722544+wesleyrj@users.noreply.github.com>
2023-01-12 11:18:58 -0300

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+     <div className="header">
+        <div className="cabecalho">
+        {/* logo */}
+        {/* Marcas */}
+        {/* Comprar */}
+        {/* Vender */}
+        {/* Avaliar */}
+        {/* Troca de Carro */}
+        {/* Contatos */}
+        </div>
+
+     </div>
+    )
+  }
+}
