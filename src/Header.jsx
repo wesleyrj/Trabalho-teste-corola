44efb75c658301ce21f1df914efccfd584949e3f
wesleyrj <115722544+wesleyrj@users.noreply.github.com>
2023-01-13 11:33:38 -0300

    Add files via upload

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,6 +6,23 @@ export default class Header extends Component {
     return (
      <div className="header">
         <div className="cabecalho">
+        <nav role="navigation" class="primary-navigation">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Comprar </a>
+      <ul class="dropdown">
+        <li><a href="#">Chevrolet</a></li>
+        <li><a href="#">Ford</a></li>
+        <li><a href="#">Mercedes/</a></li>
+        <li><a href="#">Jeep</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Vender</a></li>
+    <li><a href="#">Troca</a></li>
+    <li><a href="#">Contato</a></li>
+  </ul>
+  <img src="" alt="" />
+</nav>
         {/* logo */}
         {/* Marcas */}
         {/* Comprar */}
